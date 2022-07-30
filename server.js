72689195baca2ddf3d0c5015fc996be28d68a394
Marie <mcari1311@gmail.com>
2022-07-30 12:18:08 -0400

    My server is set up and running.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3003
 
+app.get('/', function (req, res) {
+    res.send('Welcome to the Pokemon App!')
+})
 
 app.listen(3000, function() {
     console.log('Listening on port', port)
